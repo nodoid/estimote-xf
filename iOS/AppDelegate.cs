a0a13de3435e5090b9d70447dc9ed8a54d34b98e
Paul Johnson <paul@snuberry-software.co.uk>
2015-10-18 11:26:55 +0100

    lots of changes

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -2,20 +2,33 @@
 using UIKit;
 using Estimote;
 using Xamarin.Forms;
+using CoreLocation;
+using System;
 
 namespace estimote.iOS
 {
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        public static AppDelegate Self { get; set; }
+
+        public CLLocationManager LocationManager { get; set; }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            AppDelegate.Self = this;
             global::Xamarin.Forms.Forms.Init();
-
+            App.ScreenSize = new Size(UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
             LoadApplication(new App());
 
-            //Config.Setup(App.Self.APP_ID, App.Self.APP_TOKEN);
-            App.ScreenSize = new Size(UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
+            LocationManager = new CLLocationManager();
+            var iOSVersion = UIDevice.CurrentDevice.SystemVersion.Split('.');
+            if (Convert.ToInt32(iOSVersion[0]) >= 8)
+            {
+                LocationManager.RequestAlwaysAuthorization();
+                LocationManager.RequestWhenInUseAuthorization();
+            }
+
 
             return base.FinishedLaunching(app, options);
         }
